e59acff12a51508a94fc39ff2b9c98b8ecfdc2b1
Darran Kelinske <darran.kelinske@gmail.com>
2020-01-07 17:12:39 -0600

    Add pseudocode

--- app/src/main/java/com/whereisdarran/autobootcontentprovider/MainActivity.java
+++ app/src/main/java/com/whereisdarran/autobootcontentprovider/MainActivity.java
@@ -3,12 +3,9 @@ package com.whereisdarran.autobootcontentprovider;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.database.Cursor;
-import android.database.sqlite.SQLiteOpenHelper;
-import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 
-import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,29 +14,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Uri autoBootUri = Uri.parse("content://com.archer.autobootprovider/autobootinfo");
-        Uri launcherFavoritesUri = Uri.parse("content://com.teslacoilsw.launcher.settings/favorites");
+        // Pseudocode
+        // Copy database from secure location on lenovo
+        // Remove autoboot grant from all products
+        // Grant autoboot to selected product
 
 
-        DatabaseHelper sqLiteOpenHelper = new DatabaseHelper(this);
-//        try {
-//            sqLiteOpenHelper.createDatabase();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-        Cursor cursor = sqLiteOpenHelper.getReadableDatabase().rawQuery("select * from autobootinfo where packagename='fs.cpay'", null);
+        DatabaseHelper databaseHelper = new DatabaseHelper(this);
+        Cursor cursor = databaseHelper.getReadableDatabase().rawQuery("select * from autobootinfo where packagename='fs.cpay'", null);
         Log.d("darran","The cursor size is not empty: " + cursor.moveToFirst());
-        cursor = sqLiteOpenHelper.getWritableDatabase().rawQuery("UPDATE autobootinfo set grant=1 where packagename='fs.cpay'", null);
+        cursor = databaseHelper.getWritableDatabase().rawQuery("UPDATE autobootinfo set grant=1 where packagename='fs.cpay'", null);
         cursor.moveToFirst();
         cursor.close();
 
-
-//        Cursor cursor = getContentResolver().query(autoBootUri, null, "packagename=?", new String[]{"fs.cpay"}, null);
-//        if (cursor.moveToFirst()) {
-//            ContentValues contentValues = new ContentValues();
-//            contentValues.put("grant", 0);
-//            getContentResolver().update(autoBootUri, contentValues, "packagename=?", new String[]{"fs.cpay"});
-//        }
-//        cursor.close();
     }
 }
