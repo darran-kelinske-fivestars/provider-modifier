60c8b964b6b9453d168824a0f8cc28ff8e050e83
Darran Kelinske <darran.kelinske@gmail.com>
2020-01-08 10:04:19 -0600

    Working example of setting autoboot for cpay

--- app/src/main/java/com/whereisdarran/autobootcontentprovider/MainActivity.kt
+++ app/src/main/java/com/whereisdarran/autobootcontentprovider/MainActivity.kt
@@ -1,25 +1,36 @@
 package com.whereisdarran.autobootcontentprovider
 
+import android.database.SQLException
+import android.database.sqlite.SQLiteDatabase
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.fivestars.rootutil.RootUtil
+import java.io.File
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Pseudocode
-        // Copy database from secure location on lenovo
-        // Remove autoboot grant from all products
-        // Grant autoboot to selected product
-        val databaseHelper = DatabaseHelper(this)
-        var cursor = databaseHelper.readableDatabase.rawQuery("select * from autobootinfo where packagename='fs.cpay'", null)
-        Log.d("darran", "The cursor size is not empty: " + cursor.moveToFirst())
-        cursor = databaseHelper.writableDatabase.rawQuery("UPDATE autobootinfo set grant=1 where packagename='fs.cpay'", null)
+
+        val copiedDatabase: File = File.createTempFile("autoboot", "db", cacheDir)
+        RootUtil.executeAsRoot("cp $AUTOBOOT_DATABASE_PATH ${copiedDatabase.absolutePath}")
+        val sqLiteDatabase = openDataBase(copiedDatabase)
+        val cursor = sqLiteDatabase.rawQuery("UPDATE autobootinfo set grant=1 where packagename= ?", arrayOf("fs.cpay"))
         cursor.moveToFirst()
         cursor.close()
-        RootUtil.executeAsRoot("asdf")
+
+        RootUtil.executeAsRoot("cp ${copiedDatabase.absolutePath} $AUTOBOOT_DATABASE_PATH")
+    }
+
+    @Throws(SQLException::class)
+    fun openDataBase(database: File): SQLiteDatabase {
+        return SQLiteDatabase.openDatabase(database.absolutePath, null,
+                SQLiteDatabase.OPEN_READWRITE)
+    }
+
+    companion object {
+        private const val AUTOBOOT_DATABASE_PATH = "/data/data/com.archer.providers.autoboot/databases/autoboot.db"
     }
 }
